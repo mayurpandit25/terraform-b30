8a002c3e017d440d3cf5a63b226aeb672a17a209
mayurpandit25 <panditmayur888@gmail.com>
2026-06-19 09:01:46 +0530

    day-8

--- Day-8/main.tf
+++ Day-8/main.tf
@@ -1,1 +1,15 @@
-resource "aws_s3_bucket" "s3" {}
+resource "aws_instance" "instance" {
+    for_each = tomap({
+        server-1 = "t3.micro"
+        server-2 = "t3.small"
+        server-3 = "c7i-flex.large"
+    })
+
+    ami = "ami-0741dc526e1106ae5"
+    instance_type = each.value 
+    tags = {
+        Name = each.key
+    }
+}
+
+
